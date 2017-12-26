79e1b1857734500f5372b6df9a5485f87037e962
MEISHUWEI <lineage3337@qq.com>
2017-12-26 15:10:12 +0800

    梅淑伟+作业提交

---
+++ src/TenTraning/MobileExeption.java
@@ -0,0 +1,32 @@
+package TenTraning;
+
+public class MobileExeption extends RuntimeException {
+
+	public MobileExeption() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public MobileExeption(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MobileExeption(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MobileExeption(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MobileExeption(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
---
+++ src/TenTraning/MobileTest.java
@@ -0,0 +1,46 @@
+package TenTraning;
+
+import java.util.*;
+
+public class MobileTest {
+
+	private static String mobile;
+
+	public static void judgeMobile() {
+		if (mobile.length() < 11 && mobile.length() > 0) {
+			// 抛出业务异常
+			throw new MobileExeption("手机位数不对!");
+		} else if (mobile == null || mobile.length() <= 0)// 手机号码不能为空
+		{
+			throw new MobileExeption("手机号不能为空!");
+		} else if(mobile.matches("^[a-zA-Z]+$"))
+		{
+			throw new MobileExeption("手机号必须为数字!");
+		}
+		else if (mobile.matches("^[0-9]*$"))// 手机号码里必须为数字。。。。。
+		{
+			System.out.println("输入正确!");
+			System.out.println("手机号码为: " + mobile);
+
+		} else {
+			throw new MobileExeption("手机号必须为数字!");
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("请输入手机号码: ");
+		Scanner sc = new Scanner(System.in);
+		//
+		mobile = sc.nextLine();
+
+		try {
+			judgeMobile();
+		} catch (MobileExeption ce) {
+			System.out.println(ce.getMessage());
+		}
+
+	}
+
+}
